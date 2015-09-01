acd321cd434926e3e498c4e9c209e5d357646ac0
MichielStock <michiel.stock@telenet.be>
2015-09-01 10:37:37 +0200

    file for network inference

---
+++ Network_inference/networks_evaluation.py
@@ -0,0 +1,44 @@
+"""
+Created on Tue Sep 1 2015
+Last update: -
+
+@author: Michiel Stock
+michielfmstock@gmail.com
+
+Module to check efficiency of a network inference algorithm
+"""
+
+import numpy as np
+from random import shuffle, choice
+
+class PermutationTester():
+    """
+    Class for testing reestimation of sparse non-negative matrices
+    Randomly swaps a certain fraction of the negative vs positive observations
+    and tests the reconstruction
+    """
+    def __init__(self, Y):
+        self.nrows, self.ncols = Y.shape
+        self.Y = Y
+        self.n_positive = np.sum(Y>0)
+
+    def swap_labels(self, fraction_pos):
+        """
+        Swaps a certain fraction of the positive labels with the
+        negative labels
+        Uses replacement
+        """
+        assert fraction_pos > 0 and fraction_pos < 1
+        self.n_swap = int(self.n_positive * fraction_pos)
+        positive_entries = [(self.Y[i,j], i , j) for i in range(self.nrows)\
+                for j in range(self.ncols) if self.Y[i,j] > 0]
+        zero_entries = [(self.Y[i,j], i , j) for i in range(self.nrows)\
+                for j in range(self.ncols) if not self.Y[i,j] > 0]
+        shuffle(positive_entries)
+        shuffle(zero_entries)
+        self.positive_entries = positive_entries[:self.n_swap]
+        self.zero_entries = zero_entries[:self.n_swap]
+        self.Y_swapped = self.Y.copy()
+        for (v1, i1, j1), (v2, i2, j2) in zip(self.positive_entries, self.zero_entries):
+            self.Y_swapped[i1, j1] = v2
+            self.Y_swapped[i2, j2] = v1
