cca9ef9ffaec53cae1d4b300b7519c49b28cc1f8
MichielStock <michiel.stock@telenet.be>
2016-04-11 15:24:39 +0200

    Added file to calculate alignment-independent features

--- Network_inference/networks_evaluation.py
+++ Network_inference/networks_evaluation.py
@@ -11,6 +11,8 @@ Module to check efficiency of a network inference algorithm
 import numpy as np
 from random import shuffle, choice
 
+root_mean_squared_error = lambda x, y : np.mean((x-y)**2)**0.5
+
 class PermutationTester():
     """
     Class for testing reestimation of sparse non-negative matrices
@@ -42,3 +44,15 @@ class PermutationTester():
         for (v1, i1, j1), (v2, i2, j2) in zip(self.positive_entries, self.zero_entries):
             self.Y_swapped[i1, j1] = v2
             self.Y_swapped[i2, j2] = v1
+
+    def score(self, predictions, metric=root_mean_squared_error):
+        """
+        Compares the predictions with the origanal values
+        """
+        return metric(self.Y, predictions)
+
+if __name__ == '__main__':
+    Y = np.random.binomial(3, 0.3, size=(100, 50))
+    per_tester = PermutationTester(Y)
+    per_tester.swap_labels(0.05)
+    print(per_tester.score(per_tester.Y_swapped))
